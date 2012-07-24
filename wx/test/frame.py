ed546862d5dcb0a68acad04f4c662f02774c2b73
Abraham Toriz Cruz <abram_92@yahoo.com.mx>
2012-07-23 19:53:10 -0500

    Test of a StyledTextCtrl, use of scintilla

--- wx/test/frame.py
+++ wx/test/frame.py
@@ -1,10 +1,11 @@
 #frame.py
 import wx
+from wx import stc #Developingo 2012-07-23
 
 class my_frame(wx.Frame):
     def __init__(self, parent, title):
         wx.Frame.__init__(self, parent, title=title, size=(200, 100))
-        self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
+        self.control = stc.StyledTextCtrl(self, style=wx.TE_MULTILINE) #Developingo 2012-07-23
         self.Show(True)
 
 if __name__ == "__main__":
